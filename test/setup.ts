56a2341e39cef958b00f1e48946ea7ebe862bf7d
Sahil-4 <www.sahil.875020@gmail.com>
2026-08-16 08:54:50 +0530

    test: add setup file for Vitest with console method mocks

---
+++ test/setup.ts
@@ -0,0 +1,7 @@
+import { vi, beforeEach } from 'vitest';
+
+beforeEach(() => {
+  vi.spyOn(console, 'log').mockImplementation(() => undefined);
+  vi.spyOn(console, 'warn').mockImplementation(() => undefined);
+  vi.spyOn(console, 'error').mockImplementation(() => undefined);
+});
